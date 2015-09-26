484bcb248119064a2a94c4d5572c899526eeadc3
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2015-09-26 12:08:05 +0900

    initial commit

---
+++ goCoworkingSpaceReport/AnotherWindow.swift
@@ -0,0 +1,15 @@
+//
+//  AnotherWindow.swift
+//  goCoworkingSpaceReport
+//
+//  Created by 酒井文也 on 2015/09/25.
+//  Copyright © 2015年 just1factory. All rights reserved.
+//
+
+//一覧画面の表示状態のenum
+enum AnotherWindow {
+    
+    //現在の状態（一覧画面）
+    case HideProfile
+    case DisplayProfile
+}
